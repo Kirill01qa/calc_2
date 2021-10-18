bcd6e76a532d4b25f41b99fa6143bd591ce49484
Kirill_Shapran <kirill_shapranqa@MacBook-Pro-Kirill-Shapran.local>
2021-10-18 11:30:03 +0300

    create calc_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,37 @@
-//package com.company;
-//import java.util.Scanner;
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        Scanner reader = new Scanner(System.in);
-//        System.out.println("Enter num1: ");
-//        double num1 = reader.nextDouble();
-//
-//        System.out.println("Enter num2: ");
-//        double num2 = reader.nextDouble();
-//
-//        System.out.println("Enter operator (+,-,*,/) ");
-//        char operator = reader.next().charAt(0);
-//
-//        double result;
-//        switch (operator){
-//            case '+':
-//                result = num1+num2;
-//                break;
-//            case '-':
-//                result = num1-num2;
-//                break;
-//            case '*':
-//                result = num1*num2;
-//                break;
-//            case '/':
-//                result = num1/num2;
-//                break;
-//            default:
-//                System.out.println("Sorry! Operator is incorrect");
-//                return;
-//        }
-//        System.out.println(num1 + "" + operator + "" + num2 + " = " + result + "     Yeah Bitch!!!");
-//
-//    }
-//}
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        double num1 = reader.nextDouble();
+
+        System.out.println("Enter num2: ");
+        double num2 = reader.nextDouble();
+
+        System.out.println("Enter operator (+,-,*,/) ");
+        char operator = reader.next().charAt(0);
+
+        double result;
+        switch (operator){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Sorry! Operator is incorrect");
+                return;
+        }
+        System.out.println(num1 + "" + operator + "" + num2 + " = " + result + "     Yeah Bitch!!!");
+
+    }
+}
